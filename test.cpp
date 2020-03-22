fd751d6c06ffbe60fe48ae3f82fb0571119c66e8
OnurKader <onurkader@protonmail.com>
2020-03-22 14:43:11 +0300

    Added binary to gitignore and small string_view inputting

--- test.cpp
+++ test.cpp
@@ -4,7 +4,20 @@
 
 int main()
 {
-	OK::Token tok;
-	std::cout << "Hello World!" << std::endl;
+	OK::Token tok("1+2 * 3");
+	char* input_buffer = nullptr;
+	ssize_t read_count = 0LL;
+	size_t len = 0ULL;
+
+	while((read_count = getline(&input_buffer, &len, stdin)) != -1)
+	{
+		// Get rid of the newline at the end of stdin input
+		input_buffer[--read_count] = '\0';
+		std::string_view input(input_buffer);
+		std::cout << input << std::endl;
+	}
+
+	free(input_buffer);
+
 	return 0;
 }
